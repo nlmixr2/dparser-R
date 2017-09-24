ec5033918f3f9786f9dcb70c0ba4ef76add9f888
Fidler <matthew.fidler@gmail.com>
2017-09-23 22:33:19 -0500

    Address CRAN Comments:
    - Add examples.
    - Fix description
    - Fix github URL

--- R/dparser.R
+++ R/dparser.R
@@ -156,6 +156,10 @@ refresh <- function(){ # nocov start
 ##'
 ##' @author Matthew L. Fidler for R interface, John Plevyak for
 ##'     dparser
+##' @examples
+##'
+##' ## This makes the ANSI c grammar file to parse C code:
+##' mkdparse(system.file("ansic.test.g", package = "dparser"),"ansic_gram.c", grammar_ident="ascii_C");
 ##' @export
 mkdparse <- function(file,outputFile,
                      set_op_priority_from_rule = FALSE,
@@ -414,6 +418,33 @@ gc.dparser <- function(env){
 ##' @template garbage
 ##'
 ##' @seealso \code{\link{mkdparse}}
+##' @examples
+##' \donttest{
+##' ## This creates the R based parsing function.  It requires
+##' ## compilation and runs on most OSes, with the exception of solaris.
+##' ## Windows requires Rtools to be installed.
+##' f <- dparse(system.file("tran.g", package = "dparser"),children_first=FALSE);
+##'
+##' ## Once created, you may then use this function to parse an
+##' ## arbitrary syntax file.  For example:
+##' f("
+##' b       = -1
+##' d/dt(X) = a*X + Y*Z;
+##' d/dt(Y) = b*(Y - Z);
+##' d/dt(Z) = -X*Y + c*Y - Z
+##' if (t < 0.02 | t > 99.98){
+##'     print
+##' }
+##' ", function(name, value, pos, depth){
+##'     ## This prints the name, value, position and depth passed to the
+##'     ##parsing function.
+##'     cat(sprintf("name:%s;value:%s;pos:%s;depth:%s\n", name, value, pos,
+##'                  depth));
+##' })
+##'
+##' ## You could use a better R parsing function; You could also use
+##' ## this as a starting place for your own C-based parser
+##'}
 ##' @export
 dparse <- function(grammar,
                    start_state=0,
