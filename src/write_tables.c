5481b47923f388f3f5e68865600526efe36d0061
Matthew Fidler <matthew.fidler@gmail.com>
2022-10-31 08:10:26 -0500

    Change to null string initialization to avoid gcc warnings

--- src/write_tables.c
+++ src/write_tables.c
@@ -1532,7 +1532,7 @@ static void write_state_data(File *fp, Grammar *g, VecState *er_hash, char *tag)
 static int write_header(Grammar *g, char *base_pathname, char *tag) {
   char pathname[FILENAME_MAX];
   char ver[128];
-  snprintf(ver, 128, ""); // initialize to blank string
+  ver[0] = 0; // initialize to blank string
   uint i, tokens = 0, states = 0, col;
   FILE *hfp;
 
@@ -1721,7 +1721,7 @@ void write_parser_tables_internal(Grammar *g, char *base_pathname, char *tag, in
   file_init(&file, binary, fp, str, str_len);
   if (!binary) {
     char ver[128];
-    snprintf(ver, 128, ""); // initialize to blank string
+    ver[0]=0;// initialize to blank string
     int header = write_header(g, base_pathname, tag);
 if (d_use_r_headers) {
     fprintf(fp, "#include <R.h>\n#include <Rinternals.h>\n#define printf Rprintf\n\n");}
