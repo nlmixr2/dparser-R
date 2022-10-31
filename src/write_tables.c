5f875e7f52a4cf672102e2a620f7f4592f1cb81f
Matthew Fidler <matthew.fidler@gmail.com>
2022-10-31 08:04:19 -0500

    Change blank to blank string

--- src/write_tables.c
+++ src/write_tables.c
@@ -1532,6 +1532,7 @@ static void write_state_data(File *fp, Grammar *g, VecState *er_hash, char *tag)
 static int write_header(Grammar *g, char *base_pathname, char *tag) {
   char pathname[FILENAME_MAX];
   char ver[128];
+  snprintf(ver, 128, ""); // initialize to blank string
   uint i, tokens = 0, states = 0, col;
   FILE *hfp;
 
